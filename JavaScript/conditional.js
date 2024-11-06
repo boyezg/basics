708ff462af85cb7cea8a8e0811022d2e27e976b6
boyezg <boyezgollapalli@gmail.com>
2024-11-06 13:35:43 +0530

    Practice code

---
+++ JavaScript/conditional.js
@@ -0,0 +1,31 @@
+// if statement have only one condition that is true condition only
+
+var a = 10;
+if(a>0){
+    console.log("a is positive");
+}
+
+
+// if else statement have two condition that is true and false condition    
+
+var a = 10;
+if(a>0){
+    console.log("a is positive");
+}
+else{
+    console.log("a is negative");
+}
+
+
+// if else if statement have multiple condition that is true and false condition
+
+var a = 10;
+if(a<5){
+    console.log("a is less than 5");
+}
+else if(a>5){
+    console.log("a is greater than 5");
+}
+else{
+    console.log("a is equal to 5");
+}   
---
+++ JavaScript/functions.js
@@ -0,0 +1,28 @@
+// this is the one type of function
+
+
+function add(x,y){
+    console.log(x+y);
+}
+var a = 10;
+var b= 20;
+add(a,b);
+
+
+// this is the second type of function
+
+function sub(x,y){
+    return x+y;
+}
+var a = 10;
+var b= 20;
+console.log(sub(a,b));  
+
+// this is the third type of function
+
+function mul(x,y){
+    x=10;
+    y=20;
+    return x*y;
+}
+console.log(mul());  
